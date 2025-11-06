3faac00e95362f5fa1aef61fc9133f1c5e8bed59
Mani <mani.masteali7@gmail.com>
2025-11-05 23:23:28 -0500

    added features to server console for exercise 2 part c

--- simpleChat/edu/seg2105/edu/server/backend/ServerConsole.java
+++ simpleChat/edu/seg2105/edu/server/backend/ServerConsole.java
@@ -1,4 +1,5 @@
 package edu.seg2105.edu.server.backend;
+import java.io.IOException;
 import java.util.Scanner;
 import edu.seg2105.client.common.ChatIF;
 
@@ -20,10 +21,70 @@ public class ServerConsole implements ChatIF {
 	public void accept() {
 		try {
 			while(true) {
+			
 				String msg = fromConsole.nextLine();
+				
+				if(msg.startsWith("#")) {
+					String[] line = msg.split(" ");
+					String command = line[0];
+					switch (command) {
+					case "#quit":
+						server.close();
+						System.exit(0);
+						break;
+					case "#stop":
+						server.stopListening();
+						display("Server stopped listening for new clients.");
+						break;
+					case "#close":
+						try {
+							server.close();
+							display("Server closed. All clients disconnected.");
+						}
+						catch (IOException e) {
+							display("Error closing server: "+e.getMessage());
+						}
+						break;
+					case "#setport":
+						if (server.isListening() || server.getNumberOfClients()>0) {
+							display("Error: Server must be closed to set port.");
+						}
+						else if (line.length<2)
+						{
+							display("Usage: #setport <port>");
+						}
+						else {
+							int newPort = Integer.parseInt(line[1]);
+							server.setPort(newPort);
+							display("Port set to "+ newPort);
+						}
+						break;
+					case "start":
+						if(server.isListening()) {
+							display("Error: Server already listening");
+						}
+						else {
+							try {
+							server.listen();
+							display("Server started listening for new clients.");
+							}
+							catch(IOException e) {
+								display("Error: Could not start server");
+							}
+						}
+						break;
+					case "getport":
+						display("Current port: "+server.getPort());
+						break;
+					}
+					
+				}
+				
+				else {
 				String serverMsg = "SERVER MSG> "+ msg;
 				display(serverMsg);
 				server.sendToAllClients(serverMsg);
+				}
 			}
 		}
 		catch (Exception ex){
